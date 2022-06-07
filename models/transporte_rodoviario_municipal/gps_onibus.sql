2f301d59e636138d351e6b9428fba7398983121e
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-06-07 11:02:27 -0300

    feat: add smtr tables

---
+++ models/transporte_rodoviario_municipal/gps_onibus.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized='incremental',
+        partition_by={
+            "field": "data",
+            "data_type": "date",
+            "granularity": "day",
+        }    
+    )
+}}
+
+SELECT 
+    * -- TODO: revisar colunas que serão materializadas
+FROM `rj-smtr.br_rj_riodejaneiro_veiculos.gps_sppo`
+WHERE data < CURRENT_DATE('America/Sao_Paulo')
+
+{% if is_incremental() %}
+
+{% set max_partition = run_query("SELECT gr FROM (SELECT IF(max(data) > CURRENT_DATE('America/Sao_Paulo'), CURRENT_DATE('America/Sao_Paulo'), max(data)) as gr FROM " ~ this ~ ")").columns[0].values()[0] %}
+
+AND
+    data > ("{{ max_partition }}")
+
+{% endif %}
