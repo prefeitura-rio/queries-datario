c6a5dea4fe52a34af809dd137a8953b2bb5dc73d
gabriel-milan <gabriel.gazola@poli.ufrj.br>
2022-06-08 18:46:53 +0000

    Deploying to gh-pages from @ prefeitura-rio/queries-datario@65c502f5bfb82ad4a66dcf8f2c7ae4a8f98debb0 🚀

--- pages/js/404.js
+++ pages/js/404.js
@@ -66,12 +66,10 @@ function getQueryParameters() {
   }
   // If we have queryParameters["prefix"], set the URL.
   if (queryParameters["prefix"]) {
-    document.getElementById(
-      "btnAcessarDado"
-    ).href = `https://www.data.rio/documents/${queryParameters[
-      "prefix"
-    ]}/about`;
-  } else {
-    document.getElementById("btnAcessarDado").href = "#";
+    document.getElementById("btnAcessarDado").onclick = function() {
+      window.location.href = `https://www.data.rio/documents/${queryParameters[
+        "prefix"
+      ]}/about`;
+    };
   }
 }
