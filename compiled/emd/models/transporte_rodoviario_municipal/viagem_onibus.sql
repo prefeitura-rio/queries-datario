637702b37a022a7067b3e632e1ec91948557334e
d116626 <d116626@gmail.com>
2022-08-04 14:37:19 +0000

    Deploying to gh-pages from @ prefeitura-rio/queries-datario@10c04390a5347fb286e59d87c5bac7081b4cd4c1 🚀

---
+++ compiled/emd/models/transporte_rodoviario_municipal/viagem_onibus.sql
@@ -0,0 +1,23 @@
+
+
+SELECT 
+    data,
+    consorcio,
+    tipo_dia,
+    id_empresa,
+    id_veiculo,
+    id_viagem,
+    servico_informado as servico,
+    shape_id,
+    sentido,
+    datetime_partida,
+    datetime_chegada,
+    tempo_viagem,
+    distancia_planejada,
+    perc_conformidade_shape,
+    perc_conformidade_registros,
+    versao_modelo
+FROM rj-smtr.projeto_subsidio_sppo.viagem_completa 
+WHERE data >= "2022-06-01"
+
+-- 
