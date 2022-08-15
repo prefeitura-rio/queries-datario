9875609ce1715a627fe484c4ff60f21698b6fb83
d116626 <d116626@gmail.com>
2022-08-15 17:02:25 -0300

    feat: remove transport data limit

--- models/transporte_rodoviario_municipal/gps_onibus.sql
+++ models/transporte_rodoviario_municipal/gps_onibus.sql
@@ -11,13 +11,13 @@
 SELECT 
     * except(flag_em_operacao, flag_trajeto_correto, flag_trajeto_correto_hist, status)
 FROM `rj-smtr.br_rj_riodejaneiro_veiculos.gps_sppo`
-WHERE data < "2022-07-16"
+WHERE data < CURRENT_DATE('America/Sao_Paulo')
 
--- {% if is_incremental() %}
+{% if is_incremental() %}
 
--- {% set max_partition = run_query("SELECT gr FROM (SELECT IF(max(data) > CURRENT_DATE('America/Sao_Paulo'), CURRENT_DATE('America/Sao_Paulo'), max(data)) as gr FROM " ~ this ~ ")").columns[0].values()[0] %}
+{% set max_partition = run_query("SELECT gr FROM (SELECT IF(max(data) > CURRENT_DATE('America/Sao_Paulo'), CURRENT_DATE('America/Sao_Paulo'), max(data)) as gr FROM " ~ this ~ ")").columns[0].values()[0] %}
 
--- AND
---     data > ("{{ max_partition }}")
+AND
+    data > ("{{ max_partition }}")
 
--- {% endif %}
+{% endif %}
--- models/transporte_rodoviario_municipal/viagem_onibus.sql
+++ models/transporte_rodoviario_municipal/viagem_onibus.sql
@@ -26,13 +26,13 @@ SELECT
     perc_conformidade_registros,
     versao_modelo
 FROM rj-smtr.projeto_subsidio_sppo.viagem_completa 
-WHERE data >= "2022-06-01"
+WHERE data < CURRENT_DATE('America/Sao_Paulo')
 
--- {% if is_incremental() %}
+{% if is_incremental() %}
 
--- {% set max_partition = run_query("SELECT gr FROM (SELECT IF(max(data) > CURRENT_DATE('America/Sao_Paulo'), CURRENT_DATE('America/Sao_Paulo'), max(data)) as gr FROM " ~ this ~ ")").columns[0].values()[0] %}
+{% set max_partition = run_query("SELECT gr FROM (SELECT IF(max(data) > CURRENT_DATE('America/Sao_Paulo'), CURRENT_DATE('America/Sao_Paulo'), max(data)) as gr FROM " ~ this ~ ")").columns[0].values()[0] %}
 
--- AND
---     data > ("{{ max_partition }}")
+AND
+    data > ("{{ max_partition }}")
 
--- {% endif %}
+{% endif %}
