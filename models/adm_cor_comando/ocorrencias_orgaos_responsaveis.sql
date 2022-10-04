6dbbd8c051926063a2bdc809f3aca1281761f0ed
d116626 <d116626@gmail.com>
2022-10-04 17:42:10 -0300

    fix: ocorrencias_orgaos_responsaveis

--- models/adm_cor_comando/ocorrencias_orgaos_responsaveis.sql
+++ models/adm_cor_comando/ocorrencias_orgaos_responsaveis.sql
@@ -11,7 +11,7 @@
 
 SELECT
     *
-FROM `rj-cor.adm_cor_comando.ocorrencias_atividades`
+FROM `rj-cor.adm_cor_comando.ocorrencias_orgaos_responsaveis`
 WHERE data_particao < CURRENT_DATE('America/Sao_Paulo')
 
 {% if is_incremental() %}
