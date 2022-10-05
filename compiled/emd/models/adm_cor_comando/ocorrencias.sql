a0e4dc2816b1b8ba42db406613c272e30d2c28e3
d116626 <d116626@gmail.com>
2022-10-05 17:09:14 +0000

    Deploying to gh-pages from @ prefeitura-rio/queries-datario@eed2c576e4debf93c15ed371743f3529e8b608f8 🚀

--- compiled/emd/models/adm_cor_comando/ocorrencias.sql
+++ compiled/emd/models/adm_cor_comando/ocorrencias.sql
@@ -6,3 +6,10 @@ FROM `rj-cor.adm_cor_comando.ocorrencias`
 WHERE data_particao < CURRENT_DATE('America/Sao_Paulo')
 
 
+
+
+
+
+AND
+    data_particao > ("2022-10-04")
+
--- compiled/emd/models/adm_cor_comando/ocorrencias_orgaos_responsaveis.sql
+++ compiled/emd/models/adm_cor_comando/ocorrencias_orgaos_responsaveis.sql
@@ -1,7 +1,15 @@
 
 
+
 SELECT
     *
 FROM `rj-cor.adm_cor_comando.ocorrencias_orgaos_responsaveis`
 WHERE data_particao < CURRENT_DATE('America/Sao_Paulo')
+
+
+
+
+
+AND
+    data_particao > ("2022-10-04")
 
