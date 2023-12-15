f59fe9bfafabccdf6e43abaa8e19b35e8f6e62cd
patriciacatandi <62657143+patriciacatandi@users.noreply.github.com>
2023-12-15 14:45:20 +0000

    Deploying to gh-pages from @ prefeitura-rio/queries-datario@3e39003a13a053879f4c6d725bdb2a36e12128c5 🚀

--- compiled/emd/models/meio_ambiente_clima/quantidade_agua_precipitavel_satelite.sql
+++
@@ -1,6 +0,0 @@
-
-
-SELECT
- * 
-FROM `rj-cor.meio_ambiente_clima.quantidade_agua_precipitavel_satelite`
-
--- compiled/emd/models/meio_ambiente_clima/taxa_precipitacao_satelite.sql
+++
@@ -1,6 +0,0 @@
-
-
-SELECT
- * 
-FROM `rj-cor.meio_ambiente_clima.taxa_precipitacao_satelite`
-
