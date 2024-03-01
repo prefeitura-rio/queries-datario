ce1cc14de24aa00ec43da8668528bcb9ec77cd73
d116626 <d116626@gmail.com>
2024-03-01 13:46:09 +0000

    Deploying to gh-pages from @ prefeitura-rio/queries-datario@953be451b210141c179919bb2c0c5e20014bf3f0 🚀

---
+++ compiled/emd/models/adm_central_atendimento_1746/chamado.sql
@@ -0,0 +1,5 @@
+
+
+SELECT 
+    * except(descricao)
+FROM `rj-segovi.adm_central_atendimento_1746.chamado`
--- compiled/emd/models/administracao_servicos_publicos/chamado_1746.sql
+++
@@ -1,7 +0,0 @@
-
-
-SELECT 
-    * 
-FROM `rj-segovi.administracao_servicos_publicos.chamado_1746`
-WHERE data_particao < CURRENT_DATE('America/Sao_Paulo')
-
